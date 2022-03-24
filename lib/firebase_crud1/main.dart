533c9a8994780eeaa8fe08feb0011c97b03186f8
tayeb ahmed <tayebahmed@gmail.com>
2022-03-24 08:54:58 +0600

    create folder

---
+++ lib/firebase_crud1/main.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+void main(){
+  runApp(MyApp1());
+}
+
+class MyApp1 extends StatelessWidget {
+  const MyApp1({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+
+    );
+  }
+}
