9d095ac96ef8e888b5c5b000fbb2f680e49a3fc6
tayeb ahmed <tayebahmed@gmail.com>
2022-03-24 12:49:12 +0600

    fetch data from firebase using storedocs = []

--- lib/firebase_crud1/main.dart
+++ lib/firebase_crud1/main.dart
@@ -1,19 +1,35 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:firebase_crud/firebase_crud1/home_page.dart';
 import 'package:flutter/material.dart';
 
-void main(){
+void main() async{
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
   runApp(MyApp1());
 }
 
 class MyApp1 extends StatelessWidget {
-  const MyApp1({Key? key}) : super(key: key);
+
+  Future<FirebaseApp> initialization = Firebase.initializeApp();
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      home: HomePage1(),
+    return FutureBuilder(
+      future: initialization,
+      builder: (context,snapshot){
+        if(snapshot.hasError){
+         print('Something wrong');
+        }else if(snapshot.connectionState == ConnectionState.done){
+          return MaterialApp(
+            debugShowCheckedModeBanner: false,
+            home: HomePage1(),
 
+          );
+        }
+        return Center(
+          child: CircularProgressIndicator(),
+        );
+      },
     );
   }
 }
--- lib/firebase_crud1/student_list_page.dart
+++ lib/firebase_crud1/student_list_page.dart
@@ -1,3 +1,4 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_crud/firebase_crud1/add_student_page.dart';
 import 'package:firebase_crud/firebase_crud1/edit_student_page.dart';
 import 'package:flutter/material.dart';
@@ -10,83 +11,118 @@ class ListStudentPage1 extends StatefulWidget {
 }
 
 class _ListStudentPage1State extends State<ListStudentPage1> {
+
+  Stream<QuerySnapshot> studentsStream =
+      FirebaseFirestore.instance.collection('students').snapshots();
+
+  deleteUser(id) {
+    print('Deleted id: $id');
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Container(
-        margin: EdgeInsets.symmetric(horizontal: 10.0, vertical: 20.0),
-        child: SingleChildScrollView(
-          scrollDirection: Axis.vertical,
-          child: Table(
-            border: TableBorder.all(color: Colors.purple, width: 2.0),
-            columnWidths: <int, TableColumnWidth>{
-              1: FixedColumnWidth(140),
-            },
-            defaultVerticalAlignment: TableCellVerticalAlignment.middle,
-            children: [
-              TableRow(children: [
-                TableCell(
-                  child: Container(
-                    alignment: Alignment.center,
-                    color: Colors.green,
-                    child: Text(
-                      'Name',
-                      style: TextStyle(color: Colors.white),
+    return StreamBuilder(
+      stream: studentsStream,
+      builder: (context, AsyncSnapshot<QuerySnapshot> snapshot) {
+        if (snapshot.hasError) {
+          print('Something wrong');
+        } else if (snapshot.connectionState == ConnectionState.waiting) {
+          return Center(
+            child: CircularProgressIndicator(),
+          );
+        }
+
+        /// Fetch data from FirebaseFirestore
+        List storeDocs = [];
+        snapshot.data!.docs.map((DocumentSnapshot document) {
+          Map map = document.data() as Map<String, dynamic>;
+          storeDocs.add(map);
+        }).toList();
+
+        return Scaffold(
+          body: Container(
+            margin: EdgeInsets.symmetric(horizontal: 10.0, vertical: 20.0),
+            child: SingleChildScrollView(
+              scrollDirection: Axis.vertical,
+              child: Table(
+                border: TableBorder.all(color: Colors.purple, width: 2.0),
+                columnWidths: <int, TableColumnWidth>{
+                  1: FixedColumnWidth(140),
+                },
+                defaultVerticalAlignment: TableCellVerticalAlignment.middle,
+                children: [
+                  TableRow(children: [
+                    TableCell(
+                      child: Container(
+                        alignment: Alignment.center,
+                        color: Colors.green,
+                        child: Text(
+                          'Name',
+                          style: TextStyle(color: Colors.white),
+                        ),
+                      ),
                     ),
-                  ),
-                ),
-                TableCell(
-                  child: Container(
-                    alignment: Alignment.center,
-                    color: Colors.orange,
-                    child: Text(
-                      'Email',
-                      style: TextStyle(color: Colors.white),
+                    TableCell(
+                      child: Container(
+                        alignment: Alignment.center,
+                        color: Colors.orange,
+                        child: Text(
+                          'Email',
+                          style: TextStyle(color: Colors.white),
+                        ),
+                      ),
                     ),
-                  ),
-                ),
-                TableCell(
-                  child: Container(
-                      alignment: Alignment.center,
-                      color: Colors.purple,
-                      child: Text(
-                        'Action',
-                        style: TextStyle(color: Colors.white),
+                    TableCell(
+                      child: Container(
+                          alignment: Alignment.center,
+                          color: Colors.purple,
+                          child: Text(
+                            'Action',
+                            style: TextStyle(color: Colors.white),
+                          )),
+                    ),
+                  ]),
+                  for (var i = 0; i < storeDocs.length; i++) ...[
+                    TableRow(children: [
+                      TableCell(
+                        child: Container(
+                          child: Text(storeDocs[i]['name']),
+                          alignment: Alignment.center,
+                        ),
+                      ),
+                      TableCell(
+                        child: Container(
+                          child: Text(storeDocs[i]['email']),
+                          alignment: Alignment.center,
+                        ),
+                      ),
+                      TableCell(
+                          child: Row(
+                        children: [
+                          IconButton(
+                              onPressed: () {
+                                Navigator.push(
+                                    context,
+                                    MaterialPageRoute(
+                                        builder: (context) =>
+                                            EditStudentPage1()));
+                              },
+                              icon: Icon(Icons.add)),
+                          IconButton(
+                              onPressed: () {
+                                deleteUser(storeDocs);
+                              },
+                              icon: Icon(Icons.delete)),
+                        ],
                       )),
-                ),
-              ]),
-              TableRow(children: [
-                TableCell(
-                  child: Container(
-                    child: Text('tayeb'),
-                    alignment: Alignment.center,
-                  ),
-                ),
-                TableCell(
-                  child: Container(
-                    child: Text('tayebahmed@gmail.com'),
-                    alignment: Alignment.center,
-                  ),
-                ),
-                TableCell(
-                    child: Row(
-                  children: [
-                    IconButton(
-                        onPressed: () {
-                          Navigator.push(
-                              context,
-                              MaterialPageRoute(
-                                  builder: (context) => EditStudentPage1()));
-                        },
-                        icon: Icon(Icons.add)),
-                    IconButton(onPressed: () {}, icon: Icon(Icons.delete)),
+                    ]),
                   ],
-                )),
-              ]),
-            ],
+                ],
+              ),
+            ),
           ),
-        ),
-      ),
+        );
+      },
     );
   }
 }
